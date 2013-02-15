375fec7dc1843b15025358bf10b8dd2bb1b268a9
Jeff Hellman <jeff@hellmansoft.com>
2013-02-15 11:46:54 -0700

    Uses the second argument for the output plist.  If only two arguments, and the second is 'dict', output a dictionary to infile.plist

--- csv2plist.py
+++ csv2plist.py
@@ -14,12 +14,17 @@ if args:
 	
 	if(len(args) == 2):
 		infile = args[0]
-		outfile = infile.replace(".csv", ".plist")
-		plisttype = 'array'
+        arg2 = args[1]
+        plisttype = 'array'
+        if(arg2 == 'dict')
+            plisttype = 'dict'
+            outfile = infile.replace(".csv", ".plist")
+        else
+            outfile = args[1]
 	
 	if(len(args) == 3):
 		infile = args[0]
-		outfile = infile.replace(".csv", ".plist")
+		outfile = args[1]
 		plisttype = args[2]
 	
 	if(open(os.path.abspath(outfile),'w')):
